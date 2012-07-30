03dc56c8f2c5026d6089c14fa3560f7fd2d1926d
diogro <diogro@gmail.com>
2012-07-30 18:47:08 -0300

    pep8 compliant

--- py/pop.py
+++ py/pop.py
@@ -2,7 +2,6 @@ import numpy as np
 import random as rand
 
 
-
 class Individual:
     """class for Individuals in a Population"""
     def __init__(self, m, p, amb, mu, mu_b, sigma):
@@ -13,18 +12,16 @@ class Individual:
         self.mu_b = mu_b          # ontogenetic effects mutation rate
         self.sigma = sigma        # genetic mutation variance
 
-
     def generate(self):
         """creates an ind dict with Individual parameters"""
         b = np.zeros(2 * self.m, self.p)   # binary ontogenetic matrix
         y = np.zeros(2 * self.m)      # gene vector
-        x = b*y                  # additive effects vector
+        x = b * y                  # additive effects vector
         z = x                    # phenotipic values vector TODO incluir ruido
                                    #ambiental com variancia amb
-        return {'y': y,'x': x,'z': z,'b': b}
+        return {'y': y, 'x': x, 'z': z, 'b': b}
 
-
-    def mutate (self, ind): 
+    def mutate(self, ind):
         """Mutates an ind dict with Individual parameters"""
         for i in range(2 * self.m):
             if (rand.random() < self.mu):
@@ -34,13 +31,12 @@ class Individual:
                 if (rand.random() < self.mu_b):
                     ind['b'][i, j] = 1 - ind['b'][i, j]
 
-
     def fitness(self, ind, omega, teta):
         """calculates ind fitness from population"""
         delta_s = ind['z'] - teta
-        return np.exp(-np.dot(delta_s, np.linalg.solve(omega, delta_s))) 
+        return np.exp(-np.dot(delta_s, np.linalg.solve(omega, delta_s)))
 
-        
+
 class Population:
     """class for population of Individuals"""
     def __init__(self, n_e, teta, omega, indmod):
@@ -48,19 +44,17 @@ class Population:
         self.n_e = n_e
         self.teta = teta
         self.omega = omega
-        self.pop = [ indmod.generate() for k in range(n_e) ]
+        self.pop = [indmod.generate() for k in range(n_e)]
         self.fitness = np.ones(n_e)
 
-
     def mutate(self):
-        """mutates every individual of popualation"""
+        """mutates every individual of population"""
         for k in range(self.n_e):
             self.indmod.mutate(self.pop[k])
-    
 
     def update_fitness(self):
         """calculates the fitness of every individual of population"""
         for k in range(self.n_e):
-            self.fitness[k] = self.indmod.fitness(self.pop[k], self.omega, self.teta)
-
-   
+            self.fitness[k] = self.indmod.fitness(self.pop[k],
+                                                  self.omega,
+                                                  self.teta)
