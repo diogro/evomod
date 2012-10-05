2a2cbc46a68feeabae82c98cc7f47df5d2d4354f
diogro <diogro@gmail.com>
2012-10-05 15:16:53 -0300

    Smarter population_replicate

--- c-gsl/io_pop_functions.c
+++ c-gsl/io_pop_functions.c
@@ -18,7 +18,6 @@ void population_alloc (const int n_e, const int p, const int m,
     pop->mu_b = mu_b;
     pop->sigma = sigma;
     pop->v_e = v_e;
-
     pop->y = (gsl_vector **) malloc (pop->n_e*sizeof(gsl_vector *));
     pop->b = (gsl_matrix **) malloc (pop->n_e*sizeof(gsl_matrix *));
     pop->x = (gsl_vector **) malloc (pop->n_e*sizeof(gsl_vector *));
@@ -33,12 +32,10 @@ void population_alloc (const int n_e, const int p, const int m,
     pop->mean_b = gsl_matrix_alloc(pop->p, pop->m);
     pop->mean_x = gsl_vector_alloc(pop->p);
     pop->mean_z = gsl_vector_alloc(pop->p);
-    
     pop->g_matrix = gsl_matrix_alloc(pop->p, pop->p);
     pop->p_matrix = gsl_matrix_alloc(pop->p, pop->p);
     pop->corr_g = gsl_matrix_alloc(pop->p, pop->p);
     pop->corr_p = gsl_matrix_alloc(pop->p, pop->p);
-
     pop->theta = gsl_vector_alloc(pop->p);
     pop->omega = gsl_matrix_alloc(pop->p, pop->p);
     gsl_vector_memcpy (pop->theta, theta);
@@ -71,54 +68,6 @@ void population_free (Population *pop)
     free(pop);
 }
 
-void population_replicate (Population *dest, const Population *src)
-{
-    int i;
-    dest->n_e = src->n_e;
-    dest->p = src->p;
-    dest->m = src->m;
-    dest->burn_in = src->burn_in;
-    dest->selective = src->selective;
-    dest->mu = src->mu;
-    dest->mu_b = src->mu_b;
-    dest->sigma = src->sigma;
-    dest->v_e = src->v_e;
-    dest->y = (gsl_vector **) malloc (dest->n_e*sizeof(gsl_vector *));
-    dest->b = (gsl_matrix **) malloc (dest->n_e*sizeof(gsl_matrix *));
-    dest->x = (gsl_vector **) malloc (dest->n_e*sizeof(gsl_vector *));
-    dest->z = (gsl_vector **) malloc (dest->n_e*sizeof(gsl_vector *));
-    for (i = 0; i < pop->n_e; i++){
-        dest->y[i] = gsl_vector_alloc(dest->m);
-        dest->b[i] = gsl_matrix_alloc(dest->p, dest->m);
-        dest->x[i] = gsl_vector_alloc(dest->p);
-        dest->z[i] = gsl_vector_alloc(dest->p);
-        gsl_vector_memcpy(dest->y[i], src->y[i]);
-        gsl_matrix_memcpy(dest->b[i], src->b[i]);
-        gsl_vector_memcpy(dest->x[i], src->x[i]);
-        gsl_vector_memcpy(dest->z[i], src->z[i]);
-    }
-    pop->mean_y = gsl_vector_alloc(pop->m);
-    pop->mean_b = gsl_matrix_alloc(pop->p, pop->m);
-    pop->mean_x = gsl_vector_alloc(pop->p);
-    pop->mean_z = gsl_vector_alloc(pop->p);
-    pop->g_matrix = gsl_matrix_alloc(pop->p, pop->p);
-    pop->p_matrix = gsl_matrix_alloc(pop->p, pop->p);
-    pop->corr_g = gsl_matrix_alloc(pop->p, pop->p);
-    pop->corr_p = gsl_matrix_alloc(pop->p, pop->p);
-    pop->theta = gsl_vector_alloc(pop->p);
-    pop->omega = gsl_matrix_alloc(pop->p, pop->p);
-    gsl_vector_memcpy(dest->mean_y, src->mean_y);
-    gsl_matrix_memcpy(dest->mean_b, src->mean_b);  
-    gsl_vector_memcpy(dest->mean_x, src->mean_x);   
-    gsl_vector_memcpy(dest->mean_z, src->mean_z);   
-    gsl_matrix_memcpy(dest->g_matrix, src->g_matrix); 
-    gsl_matrix_memcpy(dest->p_matrix, src->p_matrix); 
-    gsl_matrix_memcpy(dest->corr_g, src->corr_g);   
-    gsl_matrix_memcpy(dest->corr_p, src->corr_p);   
-    gsl_vector_memcpy (dest->theta, src->theta);
-    gsl_matrix_memcpy (dest->omega, src->omega);
-}
-
 void population_copy (Population *dest, const Population *src)
 {
     int i;
@@ -147,4 +96,13 @@ void population_copy (Population *dest, const Population *src)
     gsl_matrix_memcpy(dest->corr_p, src->corr_p);   
     gsl_vector_memcpy (dest->theta, src->theta);
     gsl_matrix_memcpy (dest->omega, src->omega);
+}
+
+void population_replicate (Population *dest, const Population *src)
+{
+    int i;
+    population_alloc (src->n_e, src->p, src->m, src->burn_in, src->selective,
+            src->mu, src->mu_b, src->sigma, src->v_e, src->theta, src->omega,
+            dest);
+    population_copy (dest, src)
 }
