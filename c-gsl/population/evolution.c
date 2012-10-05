d40a8af1f06788249af5b47b579e26d53d1ce575
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-10-05 18:14:51 -0300

    Adding function to mutate a individue

--- c-gsl/population/evolution.c
+++ c-gsl/population/evolution.c
@@ -1,8 +1,42 @@
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h> 
+#include <gsl/gsl_randist.h>
 #include "pop.h"
 
-void mutate_ind(Population *pop, int ind)
+void mutate_ind(const gsl_rng *r, Population *pop, int ind)
 {
+    unsigned int mutation_num, mut_i;
+    unsigned int *b_mut_idx, *b_pos, bi;
 
+    mutation_num = gsl_ran_binomial(r, pop->mu, pop->m);
 
+    if (mutation_num > 0) {
+        for(mut_i = 0; mut_i < mutation_num; mut_i++) {
+           allele = gsl_rng_uniform_int(r, pop->m); 
+           new_allele = gsl_vector_get(pop->y[ind], allele) + gsl_ran_gaussian(r, pop->sigma); 
+           gsl_vector_set(pop->y[ind], allele, new_allele);
+        }
+    }
 
+    
+    mutation_num = gsl_ran_binomial(r, pop->mu_b, pop->m*pop->p); 
+
+    if (mutation_num > 0) {
+        b_mut_idx = (unsigned int *) malloc(pop->m*pop->n*sizeof(unsigned int));
+        b_pos = (unsigned int *) malloc(mutation_num*sizeof(unsigned int));
+        
+        for (bi = 0; bi <= pop->m*pop->n; ++bi) {
+            b_mut_idx[bi] = bi;
+        }
+
+        gsl_ran_choose(r, b_pos, mutation_num, b_mut_idx, pop->m*pop->n, sizeof(unsigned int));
+
+        for(mut_i = 0; mut_i < mutation_num; mut_i++) {
+           b_rand = gsl_rng_uniform_int(r, 1); 
+           b_x = b_pos[mut_i] % pop->m;
+           b_y = b_pos[mut_i] / pop->m; 
+           gsl_matrix_set(pop->b[ind], b_x, b_y, (gsl_matrix_get(pop->b[ind], b_x, b_y) + b_rand) % 2);
+        }
+    }
+ 
 }
