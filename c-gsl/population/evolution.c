9cfefe2a44016a7fc5b1d58fbfd78885f2f9b412
diogro <diogro@gmail.com>
2012-10-11 18:14:05 -0300

    Minor bug in population_fitness() call

--- c-gsl/population/evolution.c
+++ c-gsl/population/evolution.c
@@ -1,5 +1,5 @@
 #include <gsl/gsl_matrix.h>
-#include <gsl/gsl_vector.h> 
+#include <gsl/gsl_vector.h>
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_math.h>
@@ -18,18 +18,18 @@ void mutate_ind(const gsl_rng *r, Population *pop, const int ind)
 
     if (mutation_num > 0) {
         for(mut_i = 0; mut_i < mutation_num; mut_i++) {
-           allele = gsl_rng_uniform_int(r, pop->m); 
-           new_allele = gsl_vector_get(pop->y[ind], allele) + gsl_ran_gaussian(r, pop->sigma); 
+           allele = gsl_rng_uniform_int(r, pop->m);
+           new_allele = gsl_vector_get(pop->y[ind], allele) + gsl_ran_gaussian(r, pop->sigma);
            gsl_vector_set(pop->y[ind], allele, new_allele);
         }
     }
 
-    mutation_num = gsl_ran_binomial(r, pop->mu_b, pop->m*pop->p); 
+    mutation_num = gsl_ran_binomial(r, pop->mu_b, pop->m*pop->p);
 
     if (mutation_num > 0) {
         b_mut_idx = (unsigned int *) malloc(pop->m*pop->p*sizeof(unsigned int));
         b_pos = (unsigned int *) malloc(mutation_num*sizeof(unsigned int));
-        
+
         for (bi = 0; bi <= pop->m*pop->p; ++bi) {
             b_mut_idx[bi] = bi;
         }
@@ -37,9 +37,9 @@ void mutate_ind(const gsl_rng *r, Population *pop, const int ind)
         gsl_ran_choose(r, b_pos, mutation_num, b_mut_idx, pop->m*pop->p, sizeof(unsigned int));
 
         for(mut_i = 0; mut_i < mutation_num; mut_i++) {
-           b_rand = gsl_rng_uniform_int(r, 1); 
+           b_rand = gsl_rng_uniform_int(r, 1);
            b_x = b_pos[mut_i] % pop->m;
-           b_y = b_pos[mut_i] / pop->m; 
+           b_y = b_pos[mut_i] / pop->m;
            gsl_matrix_set(pop->b[ind], b_x, b_y, (unsigned int)(gsl_matrix_get(pop->b[ind], b_x, b_y) + b_rand) % 2);
         }
     }
@@ -62,7 +62,7 @@ void fitness_ind (Population * pop, const int ind, const gsl_matrix * omega_chol
     gsl_linalg_cholesky_solve (omega_cholesky, theta_dist, aux);
     gsl_blas_ddot (theta_dist, aux, &pop->fitness[ind]);
     pop->fitness[ind] = gsl_sf_exp((-1./2.)*(pop->fitness[ind]));
-    // TODO: inf and NaN check on fitness values 
+    // TODO: inf and NaN check on fitness values
 }
 
 void population_fitness (Population * pop)
@@ -76,11 +76,11 @@ void population_fitness (Population * pop)
     }
 }
 
-void choose_mates (const gsl_rng *r, const Population * pop, int * mates)
+void choose_mates (const gsl_rng *r, Population * pop, int * mates)
 {
     unsigned int *n, k, i, ind = 0;
     n = (unsigned int *) malloc(pop->n_e*sizeof(unsigned int));
-    population_fitness(r, pop);
+    population_fitness(pop);
     gsl_ran_multinomial (r, pop->n_e, pop->n_e, pop->fitness, n);
     for ( k = 0; k < pop->n_e; k++){
         if ( n[k] > 0){
@@ -126,8 +126,8 @@ void population_cross (const gsl_rng *r, Population * pop)
 
     /* TODO: Deixar esses caras globais? */
     int *dames, * sires;
-    gsl_vector ** new_pop_y; 
-    gsl_matrix ** new_pop_b; 
+    gsl_vector ** new_pop_y;
+    gsl_matrix ** new_pop_b;
     new_pop_y = (gsl_vector **) malloc (pop->n_e*sizeof(gsl_vector *));
     new_pop_b = (gsl_matrix **) malloc (pop->n_e*sizeof(gsl_matrix *));
     for (k = 0; k < pop->n_e; k++){
