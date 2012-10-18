36704663730bbbf4874edca490965ff236ecebc3
diogro <diogro@gmail.com>
2012-10-18 15:37:55 -0300

    fixed call to gsl_covariance

--- c-gsl/population/moments.c
+++ c-gsl/population/moments.c
@@ -3,22 +3,23 @@
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_statistics_double.h>
 #include <gsl/gsl_randist.h>
+#include <gsl/gsl_errno.h>
 #include "io.h"
 
 void covariance_calc (gsl_vector ** data, const int n_e, const int p, gsl_matrix * cov, gsl_matrix * corr)
 {
-    int k, i, j;
+    int ind, i, j;
     double data_i[n_e], data_j[n_e];
     double covariance, correlation;
 
     for( i = 0; i < p; i++){
         for ( j = 0; j <= i; j++){
-            for( k = 0; k < n_e; k++){
-                data_i[k] = gsl_vector_get(data[k], i);
-                data_j[k] = gsl_vector_get(data[k], j);
+            for( ind = 0; ind < n_e; ind++){
+                data_i[ind] = gsl_vector_get(data[ind], i);
+                data_j[ind] = gsl_vector_get(data[ind], j);
             }
-            covariance = gsl_stats_covariance(data_i, sizeof(double), data_j, sizeof(double), n_e);
-            correlation = gsl_stats_correlation(data_i, sizeof(double), data_j, sizeof(double), n_e);
+            covariance = gsl_stats_covariance(data_i, 1, data_j, 1, n_e);
+            correlation = gsl_stats_correlation(data_i, 1, data_j, 1, n_e);
             gsl_matrix_set(cov, i, j, covariance);
             gsl_matrix_set(cov, j, i, covariance);
             gsl_matrix_set(corr, i, j, correlation);
@@ -29,16 +30,16 @@ void covariance_calc (gsl_vector ** data, const int n_e, const int p, gsl_matrix
 
 void population_moments (Population *pop)
 {
-    int k;
+    int ind;
     gsl_vector_set_zero (pop->mean_y);
     gsl_matrix_set_zero (pop->mean_b);
     gsl_vector_set_zero (pop->mean_x);
     gsl_vector_set_zero (pop->mean_z);
-    for (k = 0; k < pop->n_e; k++){
-        gsl_vector_add (pop->mean_y, pop->y[k]);
-        gsl_matrix_add (pop->mean_b, pop->b[k]);
-        gsl_vector_add (pop->mean_x, pop->x[k]);
-        gsl_vector_add (pop->mean_z, pop->y[k]);
+    for (ind = 0; ind < pop->n_e; ind++){
+        gsl_vector_add (pop->mean_y, pop->y[ind]);
+        gsl_matrix_add (pop->mean_b, pop->b[ind]);
+        gsl_vector_add (pop->mean_x, pop->x[ind]);
+        gsl_vector_add (pop->mean_z, pop->y[ind]);
     }
     gsl_vector_scale (pop->mean_y, 1./pop->n_e);
     gsl_matrix_scale (pop->mean_b, 1./pop->n_e);
