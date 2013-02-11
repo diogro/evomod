9a8bb7e9c7611a61805f51709194b3a0d1c01551
diogro <diogro@gmail.com>
2013-02-10 22:34:23 -0200

    Added AVGRAtio plot

--- c-gsl/plot.script.r
+++ c-gsl/plot.script.r
@@ -1,34 +1,52 @@
-time.series.plot  <-  function(input.file, y.axis, n.traits, selection = T, corr.plot = F){
-    require(ggplot2)
+SetDataFrame  <- function(input.file, n.traits, corr.plot){
     data.init = read.table(input.file)
     if(corr.plot){
         aux.trait = rep(c(1, -2), each = n.traits/2)
         aux.trait = aux.trait%*%t(aux.trait)
         aux.trait = aux.trait[upper.tri(aux.trait)]
+        aux.selection = aux.trait
         aux.trait[aux.trait==1] = "within module 1"
         aux.trait[aux.trait==4] = "within module 2"
         aux.trait[aux.trait==-2] = "between module"
+        aux.selection[aux.selection==1] = "Positive"
+        aux.selection[aux.selection==4] = "Negative"
+        aux.selection[aux.selection==-2] = "Divergent"
         n.traits = (n.traits*n.traits-n.traits)/2
+    }
+    else{
+        aux.trait = rep(c(1, -2), each = n.traits/2)
+        aux.trait[aux.trait==1] = "module 1"
+        aux.trait[aux.trait==-2] = "module 2"
     }
     gen.number = data.init[seq(1,length(data.init[,1]),n.traits+1),]
     raw.trait.means = data.init[-seq(1,length(data.init[,1]),n.traits+1),]
     generations = length(gen.number)
     gen.number = rep(gen.number, each = n.traits)
-    if(!corr.plot){
-        sel.scheme = rep(rep(c("positive", "negative"), each=n.traits/2), generations)
+    if(corr.plot){
+        sel.scheme = rep(aux.selection, generations)
     }
     else{
-        sel.scheme = rep(aux.trait, generations)
+        sel.scheme = rep(rep(c("positive", "negative"), each=n.traits/2), generations)
     }
-    trait.name = as.factor(rep(1:n.traits))
-    data.clean = data.frame(raw.trait.means, trait.name, gen.number, sel.scheme)
-    names(data.clean) = c("main", "trait", "generation", "selection")
+    module = rep(aux.trait, generations)
+    trait.name = as.factor(rep(1:n.traits, generations))
+    data.clean = data.frame(raw.trait.means, trait.name, gen.number, sel.scheme, module)
+    names(data.clean) = c("main", "trait", "generation", "selection", "module")
+    return(data.clean)
+}
+
+time.series.plot  <-  function(input.file, y.axis, n.traits, selection = T, corr.plot = F){
+    require(ggplot2)
+    data.clean = SetDataFrame(input.file, n.traits, corr.plot)
     if (selection)
         time.series <- ggplot(data.clean, aes(generation, main, group = trait, color = selection)) + layer (geom = "point") + scale_y_continuous(y.axis)
+    else if(corr.plot)
+        time.series <- ggplot(data.clean, aes(generation, main, group = trait, color = module)) + layer (geom = "point") + scale_y_continuous(y.axis)
     else
         time.series <- ggplot(data.clean, aes(generation, main, group = trait, color = trait)) + layer (geom = "point") + scale_y_continuous(y.axis)
     return(time.series)
 }
+
 PlotPop  <- function (pop.path, n.traits){
     mean.phenotype.plot  <- time.series.plot (paste(pop.path, "phenotype.dat", sep = '/'), "mean phenotype", 10, T)
     g.var.plot <- time.series.plot (paste(pop.path, "g.var.dat", sep = '/'), "genetic variance", 10, F)
@@ -46,23 +64,36 @@ PlotPop  <- function (pop.path, n.traits){
           )
     return (plots)
 }
+
 PlotPng  <-  function(list.plots, file.name){
     require(ggplot2)
     require(gridExtra)
-    png(paste("output/images/", file.name, sep = ''), width = 1080, height = 1980)
     dir.create("output/images")
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(3, 2)))
-    vplayout <- function(x, y)
-        viewport(layout.pos.row = x, layout.pos.col = y)
-    print(list.plots[[1]]$p.corr, vp = vplayout(1, 1))
-    print(list.plots[[2]]$p.corr, vp = vplayout(1, 2))
-    print(list.plots[[3]]$p.corr, vp = vplayout(2, 1))
-    print(list.plots[[4]]$p.corr, vp = vplayout(2, 2))
-    print(list.plots[[5]]$p.corr, vp = vplayout(3, 1))
-    print(list.plots[[6]]$p.corr, vp = vplayout(3, 2))
-    dev.off()
+    file.names = names(list.plots[[1]])
+    for (i in 1:length(file.names)){
+        png(paste("output/images/", file.name, ".", file.names[i], ".png", sep = ''), width = 1080, height = 1980)
+        grid.newpage()
+        pushViewport(viewport(layout = grid.layout(3, 2)))
+        vplayout <- function(x, y)
+            viewport(layout.pos.row = x, layout.pos.col = y)
+        print(list.plots[[1]][[i]], vp = vplayout(1, 1))
+        print(list.plots[[2]][[i]], vp = vplayout(1, 2))
+        print(list.plots[[3]][[i]], vp = vplayout(2, 1))
+        print(list.plots[[4]][[i]], vp = vplayout(2, 2))
+        print(list.plots[[5]][[i]], vp = vplayout(3, 1))
+        print(list.plots[[6]][[i]], vp = vplayout(3, 2))
+        dev.off(dev.cur())
+    }
 }
+
+AVGRatioPlot <- function(input.file, y.axis, n.traits){
+    require(ggplot2)
+    data.corr = SetDataFrame(input.file, n.traits, T)
+    data.corr = aggregate(data.corr, by = list(data.corr$generation, data.corr$selection), mean)
+    time.series <- ggplot(data.corr, aes(Group.1, main, group = Group.2, color = Group.2)) + layer (geom = "point") + scale_y_continuous(y.axis)
+    return(time.series)
+}
+
 n.traits <- 10
 pop.path <- "output/burn_in"
 burnin.plots <- PlotPop(pop.path, n.traits)
@@ -83,5 +114,6 @@ for (i in 1:length(sel.strengths)){
     corridor.plots[[i]]  <- PlotPop(pop.folder, n.traits)
 }
 names(corridor.plots) = corridor.folders
-PlotPng  (corridor.plots, "corridor.png")
-PlotPng  (div.plots, "divergent.png")
+
+PlotPng  (corridor.plots, "corridor")
+PlotPng  (div.plots, "divergent")
