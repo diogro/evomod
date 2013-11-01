b87de5173e664658b66561499825ac806ca40d74
diogro <diogro@gmail.com>
2013-11-01 12:32:24 -0200

    AvgRatio and AVg plots

--- c-gsl/mod.indicator.r
+++ c-gsl/mod.indicator.r
@@ -131,9 +131,34 @@ AVGRatio <- function(mat.list, Selection_Strength, num.cores = 1){
     return(AVGRatio)
 }
 
-AVGRatioPlot  <- function(pop.list, num.cores = 10){
-    avg.data <- ldply(pop.list, function(x) AVGRatio(x$p.cor, x$selection.strength, num.cores = num.cores), .progress = 'text')
-    return(avg.data)
+AVGRatioPlot  <- function(pop.list, modules = FALSE, num.cores = 2){
+    avg <- ldply(pop.list, function(x) AVGRatio(x$p.cor, x$selection.strength, num.cores = num.cores), .progress = 'text')
+    names(avg)[6] = "AVG_Ratio"
+    if(modules){
+        m.avg = melt(avg[,-c(2, 3, 6)], id.vars = c('.id', 'Selection_Strength'))
+        m.avg = m.avg[!((m.avg['.id'] != "Full Integration") & (m.avg['variable'] == "AVG-"))]
+        m.avg = m.avg[!((m.avg['.id'] == "Full Integration") & (m.avg['variable'] == "AVG+"))]
+        avg.plot = ggplot(m.avg, aes(Selection_Strength,
+                                     value,
+                                     group=interaction(variable, Selection_Strength, .id),
+                                     colour=interaction(.id, variable))) +
+                            layer(geom="boxplot") +
+                            labs(x="Selection Strength",
+                                 y="Average Correlation",
+                                 color = "Module") +
+                            scale_colour_discrete(labels=c("Within Module 1",
+                                                           "Within Module 2",
+                                                           "Between Modules")) + theme_bw()
+
+    }
+    else{
+        avg.full = avg[avg['.id'] == "Full Integration",-3]
+        avg.plot = ggplot(avg.full, aes(Selection_Strength,
+                                        Avg_Ratio,
+                                        group = Selection_Strength)) + layer(geom="boxplot") +
+                            labs(x="Selection Strength", y="AVG Ratio") + theme_bw()
+    }
+    return(avg.plot)
 }
 
 CalcCorrOmega <- function(mat.list){
