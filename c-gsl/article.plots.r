52d60440d18c087dcc0894c5b4033f638ddbdbad
diogro <diogro@gmail.com>
2014-05-28 15:00:35 +0400

    new craayzaasplots

--- c-gsl/article.plots.r
+++ c-gsl/article.plots.r
@@ -1,6 +1,17 @@
-#load("./corridor.Rdata")
+load("./rdatas/corridor.Rdata")
 
-#load("./div.sel.Rdata")
+devtools::install_github('Evomod-R', 'diogro')
+library(EvomodR)
+library(ggplot2)
+modules.corridor = AVGRatioPlot(main.data.corridor, TRUE)
+modules.corridor = modules.corridor + theme_bw() +
+  theme(legend.position = c(0, 1),
+        legend.justification = c(0, 1),
+        legend.background = element_rect(fill="transparent"))
+ggsave("~/lg.avg.corridor.tiff", width= 8.7, height = 10, units =  "cm", dpi = 600)
+
+
+load("./rdatas/div.sel.Rdata")
 
 #nd = LastGenStatMultiPlot(main.data.div.sel, MapEffectiveDimension, "Effective Dimensionality") + theme_bw()
 #ggsave("~/lg.nd.tiff")
@@ -10,12 +21,44 @@
 #ggsave("~/lg.flex.tiff")
 #evol = LastGenStatMultiPlot(main.data.div.sel, function(mat.list) CalcIsoStat(mat.list, Evolvability), "Evolvability") + theme_bw()
 #ggsave("~/lg.evol.tiff")
-#auto = LastGenStatMultiPlotWithMean(main.data.div.sel, Autonomy, "Autonomy") + theme_bw()
-#ggsave("~/lg.auto.tiff")
-#avg.ratio = AVGRatioPlot(main.data.div.sel) + theme_bw()
-#ggsave("~/lg.avgratio.tiff")
-#corr.omega = LastGenStatMultiPlot(main.data.div.sel, CalcCorrOmega, "Fitness Surface Correlation") + theme_bw()
-#ggsave("~/lg.corr.omega.tiff")
+
+install.packages('gridExtra')
+library(gridExtra)
+auto = LastGenStatMultiPlotWithMean(main.data.div.sel, Autonomy, "Autonomy")
+auto = auto + theme_bw() +
+theme(axis.text.x = element_text(angle = 45, hjust = 1),
+      legend.position = c(1, 0),
+      legend.justification = c(1, 0),
+      legend.background = element_rect(fill="transparent"),
+      legend.title = element_text(""))
+ggsave("~/lg.auto.tiff", width= 8.7, height = 10, units =  "cm", dpi = 600)
+
+avg.ratio = AVGRatioPlot(main.data.div.sel)
+avg.ratio = avg.ratio  + theme_bw() +
+theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = c(1, 0),
+        legend.justification = c(1, 0),
+        legend.background = element_rect(colour = "black"))
+ggsave("~/lg.avgratio.tiff", width= 8.7, height = 10, units =  "cm", dpi = 600)
+
+corr.omega = LastGenStatMultiPlot(main.data.div.sel, CalcCorrOmega, "Fitness Surface Correlation")
+corr.omega = corr.omega + theme_bw() +
+theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = c(1, 0),
+        legend.justification = c(1, 0),
+        legend.background = element_rect(colour = "black"))
+ggsave("~/lg.corr.omega.tiff", width= 8.7, height = 10, units =  "cm", dpi = 600)
+tiff("~/divergent_plot.tiff", height = 7, width = 17.8, units="cm", res = 600)
+divergent_plot = grid.arrange(avg.ratio, corr.omega, auto, ncol = 3)
+dev.off()
+
+corr.omega.RS = LastGenStatMultiPlot(main.data.div.sel, CalcCorrOmegaRS, "Fitness Surface Correlation")
+corr.omega.RS= corr.omega.RS + theme_bw() +
+theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = c(1, 0),
+        legend.justification = c(1, 0),
+        legend.background = element_rect(colour = "black"))
+ggsave("~/lg.corr.omega.RS.tiff", width= 8.7, height = 10, units =  "cm", dpi = 600)
 
 #r2 = StatMultiPlot(main.data.div.sel, MapCalcR2, "Mean Squared Correlations") + theme_bw()
 #ggsave("~/ts.r2.tiff")
@@ -30,7 +73,7 @@
 #corr.omega = StatMultiPlot(main.data.div.sel, CalcCorrOmega, "Fitness Surface Correlation") + theme_bw()
 #ggsave("~/ts.corr.omega.tiff")
 
-#load("./stabilizing.Rdata")
+load("./rdatas/stabilizing.Rdata")
 #stab.corr.omega = NoSelStatMultiPlot(main.data.stabilizing, CalcCorrOmega, "Fitness Surface Correlation") + theme_bw()
 #ggsave("~/tiffs/ts.stab.corr.omega.tiff")
 #stab.r2 = NoSelStatMultiPlot(main.data.stabilizing, MapCalcR2, "Mean Squared Correlations") + theme_bw()
@@ -44,7 +87,7 @@
 #stab.avg.ratio = NoSelStatMultiPlot(main.data.stabilizing, CalcAVGRatio, "AVGRatio") + theme_bw()
 #ggsave("~/tiffs/ts.stab.avgratio.tiff")
 
-#load("./drift.Rdata")
+load("./rdatas/drift.Rdata")
 #drift.corr.omega = NoSelStatMultiPlot(main.data.drift, CalcCorrOmega, "Fitness Surface Correlation") + theme_bw()
 #ggsave("~/tiffs/ts.drift.corr.omega.tiff")
 #drift.r2 = NoSelStatMultiPlot(main.data.drift, MapCalcR2, "Mean Squared Correlations") + theme_bw()
@@ -68,12 +111,21 @@
 #ggsave("~/tiffs/ts.drift.stab.evol.tiff")
 #drift.stab.auto = NoSelStatMultiPlotMultiPop(main.data.drift, main.data.stabilizing, function(mat.list) CalcIsoStat(mat.list, Autonomy), "Directional Autonomy") + theme_bw()
 #ggsave("~/tiffs/ts.drift.stab.auto.tiff")
-#drift.stab.avg.ratio = NoSelStatMultiPlotMultiPop(main.data.drift, main.data.stabilizing, CalcAVGRatio, "AVGRatio") + theme_bw()
-#ggsave("~/tiffs/ts.drift.stab.avgratio.tiff")
+drift.stab.avg.ratio = NoSelStatMultiPlotMultiPop(main.data.drift, main.data.stabilizing, AVGRatioSimple, "AVGRatio")
+save(drift.stab.avg.ratio, file= "./rdatas/drift.stab.avg.ratio.Rdata")
+drift.stab.avg.ratio = drift.stab.avg.ratio + theme_bw() +
+  theme(legend.position = c(1, 1),
+        legend.justification = c(1, 1),
+        legend.background = element_rect(fill="transparent"))
+ggsave("~/ts_drift_stab_avgratio.tiff", width= 12, height = 10, units =  "cm", dpi = 600)
 
-# burn.in.pop = ReadFolder("burn_in", sel.type = "burn.in", direct.sel=F)
-#burn.in.avg = PlotCorrs(burn.in.pop$p.cor)
-#ggsave("~/Dropbox/labbio/articles/EvoMod\ -\ article/images/burnin.p.avg.corr.tiff")
+burn.in.pop = ReadFolder("burn_in", sel.type = "burn.in", direct.sel=F)
+burn.in.avg = PlotCorrs(burn.in.pop$p.cor)
+burn.in.avg = burn.in.avg + theme_bw() +
+  theme(legend.position = c(0, 1),
+        legend.justification = c(0, 1),
+        legend.background = element_rect(fill="transparent"))
+ggsave("~/burnin.p.avg.corr.tiff")
 ##Creating relevant plot objects
 
 #source('mod.indicator.r')
